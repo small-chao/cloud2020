25b30483949196ca4445665553c1263785756663
11948 <1194875681@qq.com>
2020-03-06 15:01:33 +0800

    刚刚构建完order和payment

---
+++ springcloud2020/cloud-api-commons/src/main/java/com/springcloud/entities/CommonResult.java
@@ -0,0 +1,23 @@
+package com.springcloud.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data  //getter setter 等方法
+//@AllArgsConstructor  //所有参数构造方法
+@NoArgsConstructor   //无参
+public class CommonResult<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResult(Integer code, String message) {
+        this(code, message,null);
+    }
+
+    public CommonResult(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
